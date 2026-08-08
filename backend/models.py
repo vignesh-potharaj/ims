aab4196b6e5c7d50b03d0afdca0c950c88ac9fcf
vignesh-potharaj <23n31a2137@mrcet.ac.in>
2026-08-08 22:57:34 +0530

    feat(models): update Product model to increase name length and fix unit_price type

--- backend/models.py
+++ backend/models.py
@@ -7,9 +7,21 @@ class Product(db.Model):
     __tablename__ = 'products'
     product_id = db.Column(db.String, primary_key=True)
     sku = db.Column(db.String, unique=True, nullable=False)
-    name = db.Column(db.String(10), nullable=False)
+    name = db.Column(db.String(100), nullable=False)
     category = db.Column(db.String(1), nullable=False)
     quantity = db.Column(db.Integer, nullable=False, default=0)
     reorder_point = db.Column(db.Integer, nullable=False, default=0)
-    unit_price = db.Column(db.FLoat, nullable=False, default=0.0)
+    unit_price = db.Column(db.Float, nullable=False, default=0.0)
     warehouse = db.Column(db.String(50), nullable=False)
+
+def to_dict(self):
+    return{
+        "productId": self.product_id,
+        "sku": self.sku,
+        "name": self.name,
+        "category": self.category,
+        "quantity": self.quantity,
+        "reorderPoint": self.reorder_point,
+        "unitPrice": self.unit_price,
+        "warehouse": self.warehouse
+    }
